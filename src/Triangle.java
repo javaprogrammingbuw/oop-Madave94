045abf3dfee1e325aea888279421ad92fd650557
Madave94 <david.tschirschwitz@uni-weimar.de>
2019-04-12 22:06:09 +0200

    Triangle class structure

---
+++ src/Triangle.java
@@ -0,0 +1,31 @@
+
+public class Triangle implements iForm{
+	
+	Line2D a,b,c;
+	
+	Triangle (Line2D a, Line2D b, Line2D c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+	
+	public boolean isValid() {
+		// TODO
+		return false;
+	}
+
+	@Override
+	public double area() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double circumference() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	
+
+}
