9318aab7c6e6ceac20fec5a464cb22a76e8d165b
Madave94 <david.tschirschwitz@uni-weimar.de>
2019-04-12 23:51:52 +0200

    added minimal-gradle file and updated gitignore

--- src/Driver.java
+++ src/Driver.java
@@ -1,3 +1,6 @@
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
 
 public class Driver {
 
@@ -6,6 +9,13 @@ public class Driver {
 		// Refer to the TestSuit
 		// Optional place to implement further methods
 		// Junit 5 was used for the TestSuit
+		Result result = JUnitCore.runClasses(TestSuit.class);
+
+		for (Failure failure : result.getFailures()) {	         
+			System.out.println(failure.toString());	      
+		}
+		
+		System.out.println(result.wasSuccessful());
 	}
 
 }
